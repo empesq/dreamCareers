a59cc047cbc699a289e99abbc2e5f869d01dc9bc
user <user@Unamed>
2014-05-25 23:06:10 +0800

    commit #2

---
+++ application/controllers/users.php
@@ -0,0 +1,22 @@
+<?php if (! defined('BASEPATH')) exit('No direct script access allowed');
+class Users extends CI_Controller {
+    
+ function __construct() {
+    parent::__construct();
+    $this->load->helper('form');
+    $this->load->helper('url');
+    $this->load->helper('security');
+    $this->load->model('Users_model');
+    $this->load->database();
+ }
+ 
+ public function index() {
+    //redirect('Users/view_users');
+     $this->view_users();
+ }
+ 
+ public function view_users() {
+    $data['query'] = $this->Users_model->get_all_users();
+    $this->load->view('view_all_users', $data);
+ }
+}
